5be3c975af974ca683abc6a9c5ee11f1cd360902
Sajeel Waien <sajeel.waien@gmail.com>
2025-10-22 11:41:29 +0500

    A3 page size changes in generate

--- generate_pdf.py
+++ generate_pdf.py
@@ -31,7 +31,7 @@ def main(input_path: str, output_filename: str, footer_tmpl: str):
 
         # 3. Inject zero‑margin @page rules *after* navigation
         page.add_style_tag(content="""
-          @page { size: A4; margin-top: 0 !important; }
+          @page { size: A3; margin-top: 0 !important; }
         """)
         
         # 5. Export with zero margins
@@ -49,23 +49,3 @@ def main(input_path: str, output_filename: str, footer_tmpl: str):
 
 if __name__ == "__main__":
     main()
-
-
-
-# with sync_playwright() as p:
-    #     browser = p.chromium.launch()
-    #     page = browser.new_page()
-    #     # point at your running React app
-    #     page.set_viewport_size({"width": 1200, "height": 800})
-    #     page.emulate_media(media="screen")
-    #     page.goto("http://localhost:5173", wait_until="networkidle")
-    #     # adjust margins or header/footer here if you like
-    #     page.pdf(
-    #         path="medical-report.pdf",
-    #         format="A4",
-    #         print_background=True,
-    #         margin={"top": "0mm", "bottom": "0mm", "left": "0mm", "right": "0mm"},
-    #         prefer_css_page_size=True,
-    #         display_header_footer=False,
-    #     )
-    #     browser.close()
